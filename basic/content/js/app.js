e6b0a66f9f03ef5b0066340fb6751c88a6291d74
prairiedrumer <prairiedrumer@gmail.com>
2017-03-31 07:44:47 -0500

    Update markup and layout structure

--- basic/content/js/app.js
+++ basic/content/js/app.js
@@ -1,42 +1,17 @@
-var Finder;
-(function (Finder) {
+var CenteralData;
+(function (CenteralData) {
     var Register = (function () {
         function Register() {
 
         }
         return Register;
     }());
-    Register.FinderLocator = angular.module('Finder', []);
 
-    Finder.FinderLocator = Register;
- 
-    Register.FinderLocator.controller("finderController", function () { return new Finder.FinderController(); });
+    Register.CenteralData = angular.module("CenteralData", []);
 
+    CenteralData.CenteralData = Register;
 
-})(Finder || (Finder = {}));
+    Register.CenteralData.controller("centeralDataController", function () { return new CenteralData.CenteralDataController(); });
 
 
-var clientId = 'YOUR CLIENT ID';
-var apiKey = 'YOUR API KEY';
-var scopes = 'https://www.googleapis.com/auth/webmasters';
-
-function auth() {
-    console.log('hi');
-    // Set the API key.
-    gapi.client.setApiKey(apiKey);
-
-    // Start the auth process using our client ID & the required scopes.
-    gapi.auth2.init({
-        client_id: clientId,
-        scope: scopes
-    })
-    .then(function () {
-        // We're authenticated, let's go...
-        // Load the webmasters API, then query the API
-        gapi.client.load('webmasters', 'v3')
-          .then(retrieveSearchesByQuery);
-    });
-}
-
-// Load the API client and auth library
-gapi.load('client:auth2', auth);
+})(CenteralData || (CenteralData = {}));
---
+++ basic/content/js/controllers/centeral-data-controller.js
@@ -0,0 +1,12 @@
+var CenteralData;
+(function (CenteralData) {
+    var CenteralDataController = (function () {
+        function CenteralDataController($scope) {
+            var vm = this;
+            this.$scope = $scope;
+        };
+        return CenteralDataController;
+    }());
+    CenteralDataController.$inject = ['$scope"'];
+    CenteralData.CenteralDataController = CenteralDataController;
+})(CenteralData || (CenteralData = {}));
